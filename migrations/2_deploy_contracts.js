0f7b1ff8f69007e6e092f0c69db1b1386239f61b
SRRC <rsivaramanbtechit@gmail.com>
2018-06-25 04:41:45 +0530

    2.Backend Completed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var RckToken = artifacts.require("./RckToken.sol");
+var RckTokenSale = artifacts.require("./RckTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(RckToken);
+  deployer.deploy(RckToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(RckTokenSale, RckToken.address, tokenPrice);
+  });
 };
